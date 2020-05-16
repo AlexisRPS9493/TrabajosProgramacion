daf235f4f1bf6fe1c5254347eca95ca6034581a4
AlexisRPS9493 <65470786+AlexisRPS9493@users.noreply.github.com>
2020-05-16 17:51:08 -0500

    Add files via upload

---
+++ TrabajoMenuVectores.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TrabajosProgramacion;
+        
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author Owner
+ */
+public class TrabajoMenuVectores {
+    
+
+   /**
+    * @param agrs the command line arguments
+    */
+
+public static void main(String[] args) {
+    int a[];
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Ingrese cuantos intentos puede realizar");
+    int size = sc.nextInt();
+    
+    
+    a = new int[size];
+    int i =0;
+    Scanner sc1 = new Scanner(System.in);
+    while(i<a.length){
+        System.out.println("Ingrese el puntaje obtenido");
+        a[i]= sc1.nextInt();
+        System.out.println(a[i]);
+        i++;
+    }}
+}
+String (estado) ="Online";
+
+switch (estado){
+    case "Puede Repetir":
+        system.out.println("Tu todavia puedes paarticipar")
+    
+    
+        
+}
+}
+    
+    
+    
+    /*}*/
+    
+    
+
+
+
+
+    
